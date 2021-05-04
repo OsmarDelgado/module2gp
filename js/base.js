f9ae34854f03dfe3c9cc7e9bdbfe62f148925996
Osmar Delgado <osmar.mas@correo.uady.mx>
2021-05-04 09:54:36 -0500

    Finished class one and exercises

--- js/base.js
+++ js/base.js
@@ -1,1 +1,34 @@
-console.log( 'Hello World!' );
+/**
+ * Exercise 1
+ * 
+ *      Get two numbers from console for operate with them
+ */
+// const num1 = parseInt( window.prompt( 'Number 1:' ) );
+// const num2 = parseInt( window.prompt( 'Number 2:' ) );
+
+// if( num1 > num2 ) {
+//     console.log(num1 / num2);
+// }
+
+// if( num1 < num2 ) {
+//     console.log(num1 + num2);
+// }
+
+// if( num1 === num2 ) {
+//     console.log(num1 * num2);
+// }
+
+/**
+ * Exercise 2
+ * 
+ *      Determine if a number is odd or even
+ */
+// const num1 = parseInt( window.prompt( 'Number 1: ' ) );
+
+// if( num1 % 2 !== 0 ) {
+//     console.log( `Number ${ num1 } is odd` );
+// }
+
+// if( num1 % 2 === 0 ) {
+//     console.log( `Number ${ num1 } is even` );
+// }
